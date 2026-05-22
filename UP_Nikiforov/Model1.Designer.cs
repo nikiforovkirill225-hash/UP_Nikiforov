7850edf25fd52fe8dad6a57788d274c97e7ce557
235002 <235002@KB37-120-C04>
2026-05-22 15:03:42 +0300

    Выполненная УП

--- UP_Nikiforov/Model1.Designer.cs
+++ UP_Nikiforov/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\kbstudent\source\repos\UP_Nikiforov\UP_Nikiforov\Model1.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\235002\source\repos\nikiforovkirill225-hash\UP_Nikiforov\UP_Nikiforov\Model1.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
